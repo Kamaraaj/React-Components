4f4762ff69e95381d2bba47cc061e1acd397f493
Kamaraaj <rajumech94@gmail.com>
2024-06-30 14:50:28 +0530

    code clean up done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import './App.css';
-import EmpList from "./employeeDashBoard/EmpList"
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import EmpCreate from './employeeDashBoard/EmpCreate';
-import EmpEdit from './employeeDashBoard/EmpEdit';
-import EmpDetails from './employeeDashBoard/EmpDetails';
+import MultiSelectFAQ from './components/MultiSelectFAQ/MultiSelectFAQ'
 
 function App() {
   return (
-    <React.Fragment>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<EmpList />} />
-        <Route path='employee'>
-        <Route path='create' element={<EmpCreate />} />
-        <Route path='edit/:empid' element={<EmpEdit />} />
-        <Route path='details/:empid' element={<EmpDetails />} />
-        </Route>
-      </Routes>
-      </BrowserRouter>
-         
-    </React.Fragment>
-
+   <MultiSelectFAQ />
   );
 }
 
